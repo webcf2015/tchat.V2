989625da04f780df24d690f421e49e3e2fddcbaf
michaelpitz <michael.pitz@cf2m.be>
2015-09-08 09:52:19 +0200

    mise en ligne de config.php, avec des valeurs locales (sécurité + intégrité des fichiers)

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+define("DBHOST", "localhost");
+define("DBLOGIN", "root");
+define("DBMDP", "");
+define("DBNAME", "tchat");
+//echo $chemin_absolu = $_SERVER['HTTP_HOST'];
+
+// chemin absolu vers le projet en cours
+define("PATH","/php/pdo/tchat/");
+
+// nb de messages affichés dans le tchat
+$nb_messages_tchat = 30;
+
